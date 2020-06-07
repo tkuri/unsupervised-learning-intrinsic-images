e637e82ac3ec2b2580040b0d083f5f58f8ba809c
tkuri <kuritateppei@gmail.com>
2020-06-07 00:19:13 -0700

    Bug fixed.

--- data/energy.py
+++ data/energy.py
@@ -2,7 +2,7 @@ import timeit
 import numpy as np
 # from ..image_util import gaussian_blur_gray_image_nz
 # from ..image_util import luminance
-from data.image_util import image_util
+import data.image_util as image_util
 # from .prob_abs_r import ProbAbsoluteReflectance
 # from .prob_abs_s import ProbAbsoluteShading
 import sys
